e322eff6fbe2168c46295a340fdf2bf42e0e0c2b
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2025-10-09 15:12:01 -0700

    chore: update ggen formula to v0.2.0 with correct SHA256 checksums

--- Formula/ggen.rb
+++ Formula/ggen.rb
@@ -5,19 +5,19 @@ class Ggen < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/seanchatmangpt/ggen/releases/download/v0.2.0/ggen-aarch64-apple-darwin.tar.gz"
-      sha256 "375fe2998a9d5fb4fcc96e079651cea97ce5e0cb8afeffd10879e4eae01137e7"
+      sha256 "4d04edaa9e386af06e3560b77023f33af0c90f2f635bbd5661b0aa8af5932a46"
     else
       url "https://github.com/seanchatmangpt/ggen/releases/download/v0.2.0/ggen-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_SHA256"
+      sha256 "7ae60304292b40e560a30746c00fa1b16bb963c4178188f45cc46a4545b98275"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/seanchatmangpt/ggen/releases/download/v0.2.0/ggen-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_SHA256"
+      sha256 "4c3bd62090e6b76a2a458cac617dcb60a9a1715bad86abaeb75d33e979f84b30"
     else
       url "https://github.com/seanchatmangpt/ggen/releases/download/v0.2.0/ggen-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_SHA256"
+      sha256 "1bb416531decbdac4f168cf606a2cac46e47cc3b21e729c421143fe7a4feb6b3"
     end
   end
   def install
