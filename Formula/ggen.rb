360be5fd05ba3b062daca355ed626923196a53b2
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-12 17:29:02 -0700

    ggen 26.8.13
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ggen.rb
+++ Formula/ggen.rb
@@ -1,11 +1,11 @@
 class Ggen < Formula
   desc "Language-agnostic, graph-aware generator for reproducible projections"
   homepage "https://github.com/seanchatmangpt/ggen"
-  version "26.8.12"
+  version "26.8.13"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.8.12/ggen-x86_64-apple-darwin.tar.gz"
-      sha256 "31b433964c54c3d17a66c2660cf78d7f8061f4c38a46e4f5978bb427742e8f04"
+      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.8.13/ggen-x86_64-apple-darwin.tar.gz"
+      sha256 "5db93c7191168df39a5bee6e7679752c0eabd445a0f0e76d073c8b5bb4989841"
     else
       url "https://github.com/seanchatmangpt/ggen/releases/download/v26.8.8/ggen-x86_64-apple-darwin.tar.gz"
       sha256 "a4304371ce787e7bfe479fdba050960cdb8761fc9ca3d272da6bd7e64af08570"
