88229bcf6d439718dc4f84120f4d55c900a323ac
ggen-homebrew-releaser <actions@users.noreply.github.com>
2026-09-23 12:57:24 +0000

    chore: update ggen formula to v26.9.19
    - macOS ARM64:  2e1f969fa5b5f7e779e3523ae9aabcfbca49797bc3c790d32662473cf7d92416
    - macOS x86_64: 304889fbffce619b1808542f9400defc5cc42856ab802258e713529f63c809b0
    - Linux ARM64:  617bba0fdd284f6468e907fdc5a6601dec38b87b6c115857090968978fdc6f7f
    - Linux x86_64: 3be2506bee89bd9b9d7dce15aa758a61c380c22c78d1f3cdea57a45530f0acab

--- Formula/ggen.rb
+++ Formula/ggen.rb
@@ -1,23 +1,23 @@
 class Ggen < Formula
   desc "Language-agnostic, graph-aware generator for reproducible projections"
   homepage "https://github.com/seanchatmangpt/ggen"
-  version "26.9.18"
+  version "26.9.19"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.9.18/ggen-aarch64-apple-darwin.tar.gz"
-      sha256 "1d06d0490e4c2638081485b674cb43d9f14018f30a15160dc2c1865dd2706751"
+      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.9.19/ggen-aarch64-apple-darwin.tar.gz"
+      sha256 "2e1f969fa5b5f7e779e3523ae9aabcfbca49797bc3c790d32662473cf7d92416"
     else
-      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.9.18/ggen-x86_64-apple-darwin.tar.gz"
-      sha256 "df931f44e7356a53983aa8e2d8ea72e989f0e3a6731c998a8eae95593aabacc7"
+      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.9.19/ggen-x86_64-apple-darwin.tar.gz"
+      sha256 "304889fbffce619b1808542f9400defc5cc42856ab802258e713529f63c809b0"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.9.18/ggen-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "776491b3971870fd91b4d8231c07677a21a91d1306873bccc8c1d075db61ea6a"
+      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.9.19/ggen-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "617bba0fdd284f6468e907fdc5a6601dec38b87b6c115857090968978fdc6f7f"
     else
-      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.9.18/ggen-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9f1d689d26c5628aa695ab775f3045387f54d17c07abb8383caf0ad88a5c09e4"
+      url "https://github.com/seanchatmangpt/ggen/releases/download/v26.9.19/ggen-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "3be2506bee89bd9b9d7dce15aa758a61c380c22c78d1f3cdea57a45530f0acab"
     end
   end
   def install
